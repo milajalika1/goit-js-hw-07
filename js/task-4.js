f68db9ffaf9901370dcd6e0a480f87fbb906542d
Анжеліка <162304677+milajalika1@users.noreply.github.com>
2024-07-25 15:37:45 +0300

    add 4 js

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,21 @@
+const registerForm = document.querySelector('.login-form');
 
+const handleSubmit = event => {
+    event.preventDefault();
+    const formEmail = registerForm.elements.email.value.trim();
+    const formPassword = registerForm.elements.password.value.trim();
+
+    if (formEmail === "" || formPassword === "") {
+        return alert ("All form fields must be filled in")
+    }
+    const formData = {
+        email: formEmail,
+        password: formPassword
+    };
+    console.log(formData);
+
+    registerForm.reset();
+
+}
+     
+registerForm.addEventListener('submit', handleSubmit);
